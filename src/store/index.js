a1540c5d3866e7cfe1ab3d457a035c224b07ed35
Aaron Pratt <aaron@empirica.fi>
2018-08-07 08:35:01 +0300

    pages setup fix

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,9 @@
 import { createStore, applyMiddleware } from 'redux';
-import { createLogger } from 'redux-logger';
+// import { createLogger } from 'redux-logger';
 import ThunkMiddleware from 'redux-thunk';
 import rootReducer from './reducers';
 
-const logger = createLogger();
+// const logger = createLogger();
 
 export default () => {
   return createStore(rootReducer, applyMiddleware(ThunkMiddleware));  // logger
